a6dffa5cd4972b10a7bcd407fd3096814eba2fbd
Walter Santos de Andrade Souza <wsasouza@hotmail.com>
2023-04-09 14:59:07 -0300

    Feat: Create Post Card

--- src/components/HomePage/Network.tsx
+++ src/components/HomePage/Network.tsx
@@ -1,5 +1,6 @@
 import useUsernameStore from '@/libs/store/useUsernameStore'
 import { Power } from '@phosphor-icons/react'
+import { Post } from '../Post'
 
 interface NetworkProps {
   username: string
@@ -7,6 +8,15 @@ interface NetworkProps {
 
 export function Network({ username }: NetworkProps) {
   const unregister = useUsernameStore((state) => state.removeUsername)
+
+  const post = {
+    id: 2573,
+    username: 'Walter',
+    created_datetime: '2023-04-08T11:06:17.878963Z',
+    title: 'First Post',
+    content:
+      'Curabitur suscipit suscipit tellus. Phasellus consectetuer vestibulum elit. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Maecenas egestas arcu quis ligula mattis placerat. Duis vel nibh at velit scelerisque suscipitt',
+  }
 
   return (
     <section className="h-screen w-full bg-zinc-100 ">
@@ -25,8 +35,8 @@ export function Network({ username }: NetworkProps) {
           </button>
         </div>
       </header>
-      <div className="max-h-[51.6rem] h-full max-w-[50rem] bg-white mx-auto  rounded-b-2xl scroll-auto">
-        <h1>Teste</h1>
+      <div className="max-h-[51.6rem] h-full max-w-[50rem] bg-white mx-auto  rounded-b-2xl overflow-hidden">
+        <Post.Card post={post} />
       </div>
     </section>
   )
---
+++ src/components/Post/Card.tsx
@@ -0,0 +1,48 @@
+import { format, formatDistanceToNow } from 'date-fns'
+import enGB from 'date-fns/locale/en-GB'
+
+interface CardProps {
+  post: {
+    id: number
+    username: string
+    created_datetime: string
+    title: string
+    content: string
+  }
+}
+
+export function Card({ post }: CardProps) {
+  const publishedDateFormatted = format(
+    new Date(post.created_datetime),
+    "d LLLL 'at' HH:mm",
+    { locale: enGB },
+  )
+
+  const publishedDateRelativeToNow = formatDistanceToNow(
+    new Date(post.created_datetime),
+    {
+      locale: enGB,
+      addSuffix: true,
+    },
+  )
+
+  return (
+    <div className="w-full max-w-[48rem] bg-slate-100 mx-auto mt-6 rounded-2xl">
+      <header className="w-full bg-accent rounded-t-2xl h-[4.375rem] flex items-center px-6">
+        <h2 className="text-[22px] text-white font-bold">{post.title}</h2>
+      </header>
+      <div className="p-6 flex items-center justify-between text-zinc-500 text-lg">
+        <span className="font-bold ">@{post.username}</span>
+        <time
+          title={publishedDateFormatted}
+          dateTime={new Date(post.created_datetime).toISOString()}
+        >
+          {publishedDateRelativeToNow}
+        </time>
+      </div>
+      <div className="px-6 pb-6 space-y-4">
+        <p>{post.content}</p>
+      </div>
+    </div>
+  )
+}
